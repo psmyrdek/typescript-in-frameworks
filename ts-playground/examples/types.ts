63d30dd4bda8349b58aad184c9135a7c0e9c4ab4
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2020-03-21 20:09:25 +0100

    Updates

--- ts-playground/examples/types.ts
+++ ts-playground/examples/types.ts
@@ -1,3 +1,9 @@
+let age = 21
+age = '99'
+
+const input: HTMLInputElement = document.querySelector('input[type="text"]')
+input.value
+
 // Converts hex to rgb, i.e. #1976d2 => [25, 118, 210]
 function toRgb(color: string) {
     if (/^#([a-z0-9]{2}){3}$/.test(color)) {
